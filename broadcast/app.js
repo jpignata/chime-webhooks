5eaebb54c20f7bcabbf5c5858e31c67b404c56a6
John Pignata <john@pignata.com>
2020-03-29 20:57:16 -0400

    Add statuspage endpoint
    This will allow updates from services like CircleCI and GitHub when they
    go have disruptions.
    
    - Move WebhookUrl to an enviornment variable

--- broadcast/app.js
+++ broadcast/app.js
@@ -1,11 +1,49 @@
 const axios = require('axios');
+const webhookUrl = process.env.WEBHOOK_URL;
 
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
-exports.broadcast = async function(event, context) {
-  await axios.post(event['WebhookUrl'], {
+exports.custom_message = async function(event, context) {
+  await axios.post(webhookUrl, {
     Content: event['Message'],
   });
 
-  console.log(`broadcast webhook_url=${event['WebhookUrl']} message=${event['Message']}`);
+  console.log(`custom_message webhook_url=${webhookUrl} message=${event['Message']}`);
+}
+
+exports.status_page = async function(event, context) {
+  const request = JSON.parse(event.body);
+
+  if (!('incident' in request)) {
+    return { 'statusCode': 200 };
+  }
+
+  let response;
+
+  try {
+    const url = `https://${request.page.id}.statuspage.io/api/v2/summary.json`;
+    response = await axios.get(url);
+  } catch (error) {
+    console.log(`statuspage error=${error}`);
+    return { 'statusCode': 500 };
+  }
+
+  const pageName = response.data.page.name
+  const incidentName = request.incident.name;
+  const url = request.incident.shortlink;
+  const status = request.incident.status;
+  const updates = request.incident.incident_updates;
+  const update = updates.sort((a, b) => b.updated_at - a.updated_at)[0];
+
+  try {
+    await axios.post(webhookUrl, {
+      Content: `/md \\[**${pageName}**\\] [${incidentName}](${url}) (${status})\n\n ${update.body}`,
+    });
+  } catch (error) {
+    console.log(`statuspage error=${error}`);
+    console.log(request)
+    return { 'statusCode': 500 };
+  }
+
+  return { 'statusCode': 200 };
 }
